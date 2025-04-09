e5921ba1a42f0d354e75cd3ffe77aa2245413eb4
Hamna22 <hamnana09@gmail.com>
2025-04-09 17:45:18 +0530

    cont

--- src/component/Wishlist.jsx
+++ src/component/Wishlist.jsx
@@ -5,6 +5,7 @@ function wishlist() {
    <>
    <h1>wishlist</h1>
    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe officiis qui temporibus explicabo quasi expedita, doloremque alias aliquam similique soluta culpa et labore corporis? Sapiente consectetur accusantium unde sed! Asperiores.</p>
+   <h6>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi vero obcaecati alias accusamus, magni esse? Laborum odio inventore molestias vel voluptatum. Itaque, repellendus vitae quod quis animi at sapiente aliquid.</h6>
    </>
     
   )
